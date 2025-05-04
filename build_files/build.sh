aa071393a9ecbd8b4a7e41c99fe6ac7fc9e5a78f
fpf <fpf@andrew.cmu.edu>
2025-05-04 13:01:13 -0400

    clean up wanted packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,37 +2,24 @@
 
 set -ouex pipefail
 
-### Install packages
-
 #' /opt`, `/usr/local`, `/mnt`, `/root` home, `/media`, + few others are symlinked to /var
 # see and example below for installations that need to write to these directories
 
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-
 #install packages for dolphin shortcuts
-dnf5 -y install jpegoptim optipng pandoc qpdf recoll  xclip foremost perl-Image-ExifTool rdfind rhash testdisk expect
-
-#microsoft fonts install
-dnf5 -y install mscore-fonts-all 
+#dnf5 -y install jpegoptim optipng pandoc qpdf recoll  xclip foremost perl-Image-ExifTool rdfind rhash testdisk expect
 
 # btrfs assistant
-dnf5 -y install btrfs-assistant
+#dnf5 -y install btrfs-assistant
 
 #for phone integration via usb
-dnf5 -y copr enable zeno/scrcpy
-dnf5 -y install scrcpy 
-dnf5 -y copr disable zeno/scrcpy
+#dnf5 -y copr enable zeno/scrcpy
+#dnf5 -y install scrcpy 
+#dnf5 -y copr disable zeno/scrcpy
 
 #copr install webapp manager from bazzite
-dnf5 -y copr enable bazzite-org/webapp-manager
-dnf5 -y install webapp-manager 
-dnf5 -y copr disable bazzite-org/webapp-manager
+#dnf5 -y copr enable bazzite-org/webapp-manager
+#dnf5 -y install webapp-manager 
+#dnf5 -y copr disable bazzite-org/webapp-manager
 
 #copr install python validity for fingerprint reader
 dnf5 -y copr enable sneexy/python-validity
@@ -50,13 +37,16 @@ curl --retry 3 -Lo /tmp/rpms/brother-udev-rule-type1-1.0.2-0.noarch.rpm "https:/
 curl --retry 3 -Lo /tmp/rpms/brmfcfaxdrv-2.0.2-1.x86_64.rpm "https://download.brother.com/welcome/dlf105190/brmfcfaxdrv-2.0.2-1.x86_64.rpm"
 
 # get and download / install bitwarden rpm
-URL=$(curl -s https://api.github.com/repos/bitwarden/clients/releases | jq -r 'first(.[] | .assets[]? | select(.browser_download_url | endswith(".rpm")) | .browser_download_url)')
-echo "Downloading Bitwarden from $URL"
-curl -sL -o /tmp/rpms/bitwarden-latest.rpm "$URL"
+#URL=$(curl -s https://api.github.com/repos/bitwarden/clients/releases | jq -r 'first(.[] | .assets[]? | select(.browser_download_url | endswith(".rpm")) | .browser_download_url)')
+#echo "Downloading Bitwarden from $URL"
+#curl -sL -o /tmp/rpms/bitwarden-latest.rpm "$URL"
 
 #install all the downloaded rpms
-cd /tmp
-ls
 dnf5 install -y /tmp/rpms/*
 mv /opt /usr/share/factory
 ln -s /var/opt /opt
+
+#microsoft fonts install
+dnf5 -y install mscore-fonts-all xorg-x11-font-utils cabextract fontconfig
+rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
