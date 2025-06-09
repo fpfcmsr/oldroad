688ad50456f8f3bdd08032b0ecdc2abe57834cd9
fpfcmsr <fpf@andrew.cmu.edu>
2025-06-08 20:08:32 -0700

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -78,7 +78,7 @@ echo "Le module acpi_call a été compilé et déplacé avec succès pour le noy
 #try to use bbswitch
 git clone https://github.com/Bumblebee-Project/bbswitch.git /tmp/bbswitch
 cd /tmp/bbswitch
-make -C /usr/src/kernels/${KERNEL} M=$(pwd) Makefile
+make -C /usr/src/kernels/${KERNEL} M=$(pwd) modules
 
 mkdir -p /usr/lib/modules/${KERNEL}/extra/bbswitch/
 mv bbswitch.ko /usr/lib/modules/${KERNEL}/extra/bbswitch/
