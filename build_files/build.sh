1eeb4f153e13a8cea1e4653cccae98c7473f0251
fpfcmsr <fpf@andrew.cmu.edu>
2025-08-11 19:26:13 -0700

    fix terra repo enable

--- build_files/build.sh
+++ build_files/build.sh
@@ -25,9 +25,9 @@ mkdir /root
 
 #get terra repo
 #dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 -y repo enable terra-release
+dnf5 -y config-manager setopt terra.enabled=1
 dnf5 -y install zed
-dnf5 -y repo disable terra-release
+dnf5 -y config-manager setopt terra.enabled=0
 
 #create directory for custom rpm download and install
 mkdir /tmp/rpms
